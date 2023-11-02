aeaa0a2d432a542d8e52c772a70a586b0d99214e
Fabefa <ich9876567@gmail.com>
2023-11-02 10:58:42 +0100

    Some col changes

--- src/main/java/net/fabefa/tutorialmod/tutorialmod.java
+++ src/main/java/net/fabefa/tutorialmod/tutorialmod.java
@@ -13,14 +13,11 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(tutorialmod.MOD_ID)
 public class tutorialmod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
     
     public tutorialmod()
     {
